09d6eda1b3afaf190af74224c91360634127cab5
kiril <kiril@DESKTOP-KK85ONR>
2024-11-09 09:43:07 +0300

    Добавьте файлы проекта.

---
+++ Lab4_rec/Lab4_rec.cpp
@@ -0,0 +1,236 @@
+﻿// Lab4_rec.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "Lab4_rec.h"
+
+#define MAX_LOADSTRING 100
+
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+
+    // Инициализация глобальных строк
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_LAB4REC, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Выполнить инициализацию приложения:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_LAB4REC));
+
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  ЦЕЛЬ: Регистрирует класс окна.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_LAB4REC));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_LAB4REC);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+void FourPointedStar(HDC hdc, int x, int y)
+{
+    MoveToEx(hdc, x, y - 80, NULL); // Вершина
+    LineTo(hdc, x + 20, y - 20);
+    LineTo(hdc, x + 80, y); // Вершина
+    LineTo(hdc, x + 20, y + 20);
+    LineTo(hdc, x, y + 80); // Вершина
+    LineTo(hdc, x - 20, y + 20);
+    LineTo(hdc, x - 80, y); // Вершина
+    LineTo(hdc, x - 20, y - 20);
+    LineTo(hdc, x, y - 80); 
+}
+
+void FivePointedStar(HDC hdc, int x, int y)
+{
+    int pointsX[] = { 0, -29, -95, -47, -58, 0, 58, 47, 95, 29 };
+    int pointsY[] = { -100, -31, -31, 19, 81, 50, 81, 19, -31, -31 };
+
+    MoveToEx(hdc, x + pointsX[0], y + pointsY[0], NULL);
+
+    for (int i = 1; i < 10; i++) {
+        LineTo(hdc, x + pointsX[i], y + pointsY[i]);
+    }
+
+    LineTo(hdc, x + pointsX[0], y + pointsY[0]);
+}
+
+void RecursiveFourStar(HDC hdc, int x, int y, int i);
+void RecursiveFiveStar(HDC hdc, int x, int y, int i);
+
+void RecursiveFourStar(HDC hdc, int x, int y, int i)
+{
+    FourPointedStar(hdc, x, y);
+    if (i < 1) {
+        return;
+    }
+    RecursiveFiveStar(hdc, x, y - 80, i - 1);
+    RecursiveFiveStar(hdc, x + 80, y, i - 1);
+    RecursiveFiveStar(hdc, x, y + 80, i - 1);
+    RecursiveFiveStar(hdc, x - 80, y, i - 1);
+}
+
+void RecursiveFiveStar(HDC hdc, int x, int y, int i)
+{
+    FivePointedStar(hdc, x, y);
+    if (i < 1) {
+        return;
+    }
+    RecursiveFourStar(hdc, x, y - 100, i - 1);
+    RecursiveFourStar(hdc, x - 95, y - 31, i - 1);
+    RecursiveFourStar(hdc, x - 58, y + 81, i - 1);
+    RecursiveFourStar(hdc, x + 58, y +81, i - 1);
+    RecursiveFourStar(hdc, x + 95, y - 31, i - 1);
+}
+
+//
+//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
+//
+//  WM_COMMAND  - обработать меню приложения
+//  WM_PAINT    - Отрисовка главного окна
+//  WM_DESTROY  - отправить сообщение о выходе и вернуться
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Разобрать выбор в меню:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            
+            RecursiveFiveStar(hdc, 800, 600, 2);
+
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Обработчик сообщений для окна "О программе".
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
